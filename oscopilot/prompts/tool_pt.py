3fdb97bee25e9bcc19db42b0a53188d300b5c7ad
Jianfeng Shao <2785984827@qq.com>
2024-11-23 14:45:08 +0800

    Add tool generate_javascript

---
+++ oscopilot/prompts/tool_pt.py
@@ -0,0 +1,10 @@
+prompt = {
+    "generate_javascript": """
+    # Generate Javascript code to complete the task {task_description}.
+
+    code output Format:
+    ```Javascript
+    Javascript code
+    ```
+    """
+}
